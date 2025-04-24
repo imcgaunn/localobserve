55f9f17c62bb7bba8e55c96f84db710a704109f3
ian mcgaunn <ian@mcgaunn.com>
2025-04-24 15:07:14 -0400

    mount local directory correctly into vector data directoryh

--- scripts/run_vector.sh
+++ scripts/run_vector.sh
@@ -50,6 +50,7 @@ function start_vector() {
         -e DD_TAGS="poc-name:localobserve" \
         --mount type=bind,src=${CONFIG_DIR}/vector.files.yaml,dst=/etc/vector/vector.yaml \
         --mount type=bind,src=${VECTOR_DATA_DIR},dst=/tmp/data \
+        --mount type=bind,src=${VECTOR_DATA_DIR},dst=/var/lib/vector \
         --mount type=bind,src=${LOG_DATA_DIR},dst=/tmp/logs \
         ${VECTOR_IMAGE_NAME}:${VECTOR_IMAGE_TAG} || die "could not create container ${VECTOR_CONTAINER_NAME}"
     docker start -ia "${VECTOR_CONTAINER_NAME}"
